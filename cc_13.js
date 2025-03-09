e33fd910d32e35cbacc58d6930ce54535a56d8c0
Mauricio Gamboa <mauriciogamboa00@gmail.com>
2025-03-09 14:37:35 -0500

    Task 5 - Inline Editing for Employee Cards

--- cc_13.js
+++ cc_13.js
@@ -1,27 +1,68 @@
 // Task 2 Adding Employee Cards Dynamically
 
-function createEmployeeCard(name, position){ 
+function createEmployeeCard(name, position) { 
 
-    const employeeContainer = document.getElementById(`employeeContainer`);
+    const employeeContainer = document.getElementById('employeeContainer');
 
-    const card = document.createElement(`div`);
+    const card = document.createElement('div');
     card.setAttribute(`class`, `employee-card`); // Setting attribures for employee cards
 
-    const heading = document.createElement(`h3`); // Setting the heading as employee name
+    const heading = document.createElement('h3'); // Setting the heading as employee name
     heading.textContent = name;
 
-    const paragraph = document.createElement(`p`); // Setting Employee postition as a paragraph
+    const paragraph = document.createElement('p'); // Setting Employee postition as a paragraph
     paragraph.textContent = position;
 
-    const removeButton = document.createElement(`button`); // Creating a remove button
-    removeButton.textContent = `Remove`;
+    const removeButton = document.createElement('button'); // Creating a remove button
+    removeButton.textContent = 'Remove';
+
+    const editButton = document.createElement('button'); // creating an edit button
+    editButton.textContent = 'Edit Employee Card'
+
+    // Task 5 Modifications
+
+    editButton.addEventListener('click', (event) => {
     
+    const nameInput = document.createElement('input'); // create an input field for employee name when edit button is clicked
+    nameInput.value = heading.textContent;
+
+    const positionInput = document.createElement('input'); // create an input field for employee position when edit button is clicked
+    positionInput.value = paragraph.textContent;
+
+    const saveButton = document.createElement('button'); // Create save button when edit employee card is clicked
+    saveButton.textContent = 'Save Changes';
+
+    saveButton.addEventListener('click', (event) => { // Adding an event listener to the save button
+        heading.textContent = nameInput.value;
+        paragraph.textContent = positionInput.value;
+        card.removeChild(saveButton) // remove save button when save chnages is clicked
+        card.removeChild(positionInput) // remove position input field when save changes is clicked
+        card.removeChild(nameInput); // remove name input field when save changes is clicked
+    })
+    
+
+
+
+    card.appendChild(nameInput); // appending name input
+    card.appendChild(positionInput); // appending position input
+    card.appendChild(saveButton); // appending the save button
+    
+    
+
+   
+
+
+    })
+
+
+
+
     // Task 4 Modifications
 
-    removeButton.addEventListener(`click`, (event) => {
+    removeButton.addEventListener('click', (event) => {
         card.remove(); // Adding an event listener to remove an employee card when it is clicked
 
-        console.log(`Employee Card Removed.`); // Log to console everytime remove button is clicked for an employee card
+        console.log('Employee Card Removed.'); // Log to console everytime remove button is clicked for an employee card
     
 
         event.stopPropagation(); // Prevent the clicks from effecting the parent container
@@ -30,6 +71,8 @@ function createEmployeeCard(name, position){
     card.appendChild(heading); // appending heading 
     card.appendChild(paragraph); // appending paragraph
     card.appendChild(removeButton); // appending remove button
+    card.appendChild(editButton); // appending edit button
+    
 
     employeeContainer.appendChild(card); // appending the employee card
     
@@ -43,9 +86,9 @@ createEmployeeCard("Mike McDaniel", "Head Coach"); // Created a new employee car
 
 // Task 3 Converting NodeLists to Arrays for Bulk Updates
 
-const cards = document.querySelectorAll(`.employee-card`); // Used query selectorAll to select all employee cards
+const cards = document.querySelectorAll('.employee-card'); // Used query selectorAll to select all employee cards
 const cardArray = [...cards]; // Converting Nodelist to an array
 
 cardArray.forEach(card => {
-card.style.backgroundColor = `orange`; // Updated the employee cards to be orange
+card.style.backgroundColor = 'orange'; // Updated the employee cards to be orange
     });
